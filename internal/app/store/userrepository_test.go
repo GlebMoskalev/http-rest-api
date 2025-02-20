a6b15310a1726d5ee0880106a2b9c2312574685a
GlebMoskalev <gleb12122003@gmail.com>
2025-02-20 14:21:27 +0500

    Add validation users model

--- internal/app/store/userrepository_test.go
+++ internal/app/store/userrepository_test.go
@@ -11,9 +11,7 @@ func TestUserRepository_Create(t *testing.T) {
 	s, tearDown := store.TestStore(t, databaseURL)
 	defer tearDown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "testuser@example.com",
-	})
+	u, err := s.User().Create(model.TestUser(t))
 
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
@@ -21,17 +19,18 @@ func TestUserRepository_Create(t *testing.T) {
 
 func TestUserRepository_FindByEmail(t *testing.T) {
 	s, tearDown := store.TestStore(t, databaseURL)
-	defer tearDown()
+
+	defer tearDown("users")
 
 	email := "testuser@example.com"
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	s.User().Create(&model.User{
-		Email: "testuser@example.com",
-	})
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
 
-	u, err := s.User().FindByEmail(email)
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
